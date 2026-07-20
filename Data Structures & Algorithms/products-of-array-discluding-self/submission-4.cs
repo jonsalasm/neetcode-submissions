4cb07b80003f8070fc11463adc5544f48601b593
jonsalasm <30638882+jonsalasm@users.noreply.github.com>
2026-07-20 15:36:22 -0500

    Add: products-of-array-discluding-self - submission-4

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-4.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums) {
+        var product = 1;
+        int[] output = new int[nums.Length];
+
+        for (int i = 0; i < nums.Length; i++) {
+            for (int j = 0; j < nums.Length; j++) {
+                if (i != j)
+                    product = nums[j] * product;
+            }
+            output[i] = product;
+            product = 1;
+        }
+
+        return output;
+    }
+}
